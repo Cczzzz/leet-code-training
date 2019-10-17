37ef38ea0a69bc7d3324d25c7f36ecdca3ef4b7d
unknown <chenchang-ds@gome.com.cn>
2019-10-18 00:07:29 +0800

    20191017

--- src/main/java/leetCode20191016.java
+++ src/main/java/leetCode20191016.java
@@ -17,12 +17,11 @@
  * Note:
  * All given inputs are in lowercase letters a-z.
  */
-public class leetCode20190603 {
+public class leetCode20191016 {
     public static void main(String[] args) {
         String[] strings = new String[]{"afaffloweraaaa", "vvflowaaaa", "11111flightaaaa"};
         System.out.println(contains("afafflower", "vv"));
         System.out.println(main0(strings));
-        System.out.println(longestCommonPrefix(strings));
     }
 
     public static String main0(String[] strings) {
@@ -85,47 +84,6 @@ public class leetCode20190603 {
             }
         }
         return false;
-    }
-
-
-    public static boolean isCommonPrefix(String[] strs, int middle) {
-        String prefix = strs[0].substring(0, middle);
-        for (int i = 1; i < strs.length; i++) {
-            if (!strs[i].startsWith(prefix)) return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * @param {string[]} strs
-     * @return {string}
-     */
-    public static String longestCommonPrefix(String[] strs) {
-        // trie 解法
-        // 时间复杂度O(m) 空间复杂度O(m * n)
-
-        // tag: 二分法
-        // 时间复杂度 O(n*logm)  空间复杂度O(1)
-        if (strs.length == 0) return "";
-        if (strs.length == 1) return strs[0];
-
-        int minLen = Integer.MAX_VALUE;
-
-        for (int i = 0; i < strs.length; i++) {
-            minLen = Math.min(minLen, strs[i].length());
-        }
-
-        int low = 0;
-        int high = minLen;
-
-        while (low <= high) {
-            int middle = (low + high) >> 1;
-            if (isCommonPrefix(strs, middle)) low = middle + 1;
-            else high = middle - 1;
-        }
-
-        return strs[0].substring(0, (low + high) >> 1);
     }
 
 }
---
+++ src/main/java/leetCode20191017.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/***
+ * There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+ *
+ * You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1).
+ * You begin the journey with an empty tank at one of the gas stations.
+ *
+ * Return the starting gas station's index if you can travel around the circuit once in the clockwise direction,
+ * otherwise return -1.
+ */
+public class leetCode20191017 {
+
+    public static void main(String[] args) {
+        int[] gas = new int[]{1, 5, 8, 7, 9, 4, 1, 9};
+        int[] cost = new int[]{5, 1, 9, 7, 1, 7, 3, 5};
+        int i = main0(gas, cost);
+        System.out.println(i);
+        if (i != -1) {
+            System.out.println(verify(i, gas, cost));
+        }
+    }
+
+    public static int main0(int[] gas, int[] cost) {
+        int start = 0;
+        int yield = 0;
+        int total = 0;
+        for (int i = 0; i < gas.length; i++) {
+            total +=  gas[i] - cost[i];
+            yield +=  gas[i] - cost[i];
+            if (yield < 0) {
+                start = i + 1;
+                yield = 0;
+            }
+        }
+        return total > 0 ? start : -1;
+    }
+
+    public static boolean verify(int start, int[] gas, int[] cost) {
+        int total = 0;
+        for (int i = start; i < gas.length; i++) {
+            total += gas[i] - cost[i];
+        }
+        for (int i = 0; i < start; i++) {
+            total += gas[i] - cost[i];
+        }
+        return total >= 0;
+    }
+
+}
